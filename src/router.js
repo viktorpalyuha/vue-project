f7047b31b9509697a479d500ef2a7979ff129ac8
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-05-02 23:46:37 +0300

    find coach project with Vue

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail';
+import CoachesList from './pages/coaches/CoachesList';
+import CoachRegistration from './pages/coaches/CoachRegistration';
+import ContactCoach from './pages/requests/ContactCoach';
+import RequestReceived from './pages/requests/RequestsReceived';
+import NotFound from './pages/NotFound';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [{ path: 'contact', component: ContactCoach }]
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound }
+  ]
+});
+
+export default router;
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'vuex';
+
+import coachesModule from './modules/coaches/index';
+import requestsModule from './modules/requests/index';
+
+const store = createStore({
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule
+  },
+  state() {
+    return {
+      userId: 'c3'
+    };
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    userId(state) {
+      return state.userId;
+    }
+  }
+});
+
+export default store;
